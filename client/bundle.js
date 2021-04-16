d1dcec659b1323f2d90a26e46e5fb26cfb650efd
Roselyn Le <roselynle95@gmail.com>
2021-04-16 17:51:55 +0100

    add functionality to feeling lucky button on client side

--- client/bundle.js
+++ client/bundle.js
@@ -7,4 +7,47 @@ searchBtn.addEventListener("click", (e) => {
     window.location.replace("../../searchResults.html");
 });
 
+// function searchButton() {
+//     document.getElementById('google-search-btn').addEventListener("click", async (e) => {
+//         e.preventDefault()
+//         const query = searchbar.value
+//         try {
+//             if (query.length < 1) throw new Error('please enter a search query')
+//             const data = await apiFuncs.getData(`http://localhost:3000/search/${query}`)
+
+//             //destroys old content
+//             const resSect = document.querySelector('#resultSection');
+//             resSect.innerHTML = "";
+
+//             appendList(data.body)
+//             handlerFuncs.layoutChange()
+//             window.location.replace("../../searchResults.html");
+//         } catch(err) {
+//             alert(err)
+//             throw err
+//         }
+//     })
+// }
+
+// searchButton()
+
+function luckyButton() {
+    document
+        .getElementById("feeling-lucky-btn")
+        .addEventListener("click", async (e) => {
+            e.preventDefault();
+            const query = document.getElementById("search").value.trim();
+            let url = `http://localhost:3000/lucky/${query}`;
+            console.log(url);
+            fetch(url).then((r) => r.json())
+            .then((content) => {
+                window.location.href = content.body.url;
+            }).catch((err) => {
+                console.log(err);
+            });
+        });
+}
+
+luckyButton();
+
 },{}]},{},[1]);
