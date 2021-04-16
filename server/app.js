e7cf76d5ad236d6f4dd4d88f5710a25c5fbee8e8
Roselyn Le <roselynle95@gmail.com>
2021-04-16 17:01:49 +0100

    feeling lucky button retrieves random search from API

--- server/app.js
+++ server/app.js
@@ -1,33 +1,52 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser')
+const axios = require('axios');
+
 const app = express();
 app.use(cors());
-app.use(express.json());
-
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("Welcome to Google Clone"));
 
 // import data
-const pizzaPlacesData = require("./data");
+// const pizzaPlacesData = require("./data");
 
 // to get the 10 results on the page
-app.get("/pizzaresult", (req, res) => res.send(pizzaPlacesData));
-
-// function getRandomNumber(min, max) {
-//     let step1 = max - min + 1;
-//     let step2 = Math.random()*step1;
-//     let result = Math.floor(step2) + min;
-
-//     return pizzaPlaces[result]}
-
-// app.get('/random', (req, res) => res.send(getRandomNumber(0, pizzaPlaces.length)));
-
-// app.get("/random", (req, res) => res.send("../client/random.html"));
+// app.get("/pizzaresult", (req, res) => res.send(pizzaPlacesData));
 
 //route to get the results for a particular index
-app.get("/pizzaresult/:id", (req, res) => {
-    const resultId = parseInt(req.params.id);
-    res.send(pizzaPlacesData[resultId - 1]);
-});
+// app.get("/pizzaresult/:id", (req, res) => {
+//     const resultId = parseInt(req.params.id);
+//     res.send(pizzaPlacesData[resultId - 1]);
+// });
+
+function randomSearch(data){
+    length = data.length
+    randomIndex = Math.floor(Math.random() * length)
+    return data[randomIndex]
+}
+
+const access_key = '0318b9b5980033df9b116f4f997ec736';
+app.get('/search/:query', (req, res) => {
+    const query = req.params.query;
+    const url = `http://api.serpstack.com/search?access_key=${access_key}&type=web&query=${query}`;
+    axios.get(url)
+    .then(function (response) {
+        res.status(200).send({body: (response.data.organic_results, 10)})
+    })
+    .catch(console.warn)
+})
+
+
+app.get('/lucky/:query', (req, res) => {
+    const query = req.params.query;
+    const url = `http://api.serpstack.com/search?access_key=${access_key}&type=web&query=${query}`;
+    axios.get(url)
+    .then(function (response) {
+        res.status(200).send({body: randomSearch(response.data.organic_results, 10)})
+    })
+    .catch(console.warn)
+})
 
 module.exports = app;
